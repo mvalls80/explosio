97b97aed5644ba8ff6f0e4a2ef4f3b1a751f45b4
mvalls80 <marcvalls08mvp@gmail.com>
2025-05-11 00:06:43 +0200

    2

--- explosio/explosio.gd
+++ explosio/explosio.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-
+@onready var explosion_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var particles: CPUParticles2D = $CPUParticles2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		$CPUParticles2D.emitting = true
-	
+		particles.emitting = true
+		explosion_sound.play()
